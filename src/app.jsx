e4ec99a04c04c35d5a787ef45a90bc18eca05486
bs23.hasan <hasan.haolader@brainstation-23.com>
2025-01-07 03:01:07 +0600

    setup project with react and tailwindcss, sass

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import "./assets/css/index.scss";
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <div className="container bg-red-100 min-h-screen flex items-center justify-center">
+        <button className="button">Click Me</button>
+      </div>
+    </div>
+  );
+}
+export default App;
