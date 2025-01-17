3f508edda49ff00acbfc4606373924ae0fff5d67
bs23.hasan <hasan.haolader@brainstation-23.com>
2025-01-18 02:36:59 +0600

    feat: handle localization

---
+++ src/components/common/locale.jsx
@@ -0,0 +1,13 @@
+const locales = {};
+function getAllLocaleFiles() {
+	const context = require.context("../../assets/lang", false, /\.json$/);
+	context.keys().forEach((fileName) => {
+		const locale = fileName.replace("./", "").replace(".json", ""); // Extract file name (e.g., 'en', 'bn')
+		locales[locale] = context(fileName);
+	});
+}
+export default function Locale({ label }) {
+	const default_lang = process.env.REACT_APP_DEFAULT_LANG;
+	getAllLocaleFiles();
+    return (<>{locales[default_lang]?.[label] || ''}</>)
+}
