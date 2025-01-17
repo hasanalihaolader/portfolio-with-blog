43da30d4a62b0843f8835b809ff70b586de041d5
bs23.hasan <hasan.haolader@brainstation-23.com>
2025-01-18 02:32:46 +0600

    feat: add new color in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+const flowbite = require("flowbite-react/tailwind");
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "body-wallpaper":
+            "url('data:image/svg+xml,%3Csvg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 32 32\" width=\"32\" height=\"32\" fill=\"none\" stroke=\"%23f1f5f9\"%3E%3Cpath d=\"M0 .5H31.5V32\"/%3E%3C/svg%3E')",
+      },
+      colors: {
+        red: {
+          default:'#f53855'
+        },
+        purple: {
+          DEFAULT: '#6D28D9'
+        },
+        green: {
+          DEFAULT: '#198754'
+        },
+        customPink: {
+          DEFAULT: '#B3489A'
+        },
+        customGray: {
+          DEFAULT: 'rgb(248 250 252)',
+        },
+        black: {
+          DEFAULT: '#000000'
+        }
+      },
+      borderWidth: {
+        '1': '1px',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    flowbite.plugin(),
+  ],
 }
